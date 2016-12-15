231ffb61a3720a053e344dc24dc6dde07f32c373
dhowe <github.utensil@mailhero.io>
2016-12-15 18:23:10 +0800

    fix to mitcheck on detail page

--- js/main.js
+++ js/main.js
@@ -18,23 +18,29 @@ function createGrid(projects) {
   }
 
   $('.project img').css("height", $('.project img').width() * 0.65);
-  adjustHeight(projects);
 
-  // navigation
-  var $navlist = $('#navlist');
-  if (location.href.indexOf('mit') > -1) {
-    $navlist.append('<li><a class="current" href="index.html">ACM/MIT: Selected Projects</a></li>');
-  }
-  else {
-    $navlist.append('<li><a class="current" href="index.html">Projects</a></li>');
-    $navlist.append('<li><a href="pubs.html">Publications</a></li>');
-    $navlist.append('<!--li><a href="sketchpad/index.html">Sketchpad</a></li-->');
-    $navlist.append('<li><a class="showInMobile" href="about.html">About</a></li>');
-  }
+  adjustHeight(projects);
+  selectNavigation();
 
   // footer space
   if ($('.footer').css("display") != "none") {
     $('#projects .grid').append("<div class='space' style='height:" + $('.footer').outerHeight() + "'></div>");
+  }
+}
+
+function selectNavigation() {
+
+  var $navlist = $('#navlist');
+  if (location.href.indexOf('mit') > -1) {
+    console.log("FOUND MIT");
+    $navlist.append('<li><a class="current" href="index.html">ACM/MIT: Selected Projects</a></li>');
+  }
+  else {
+    console.log("NOT MIT");
+    $navlist.append('<li><a class="current" href="index.html">Projects</a></li>');
+    $navlist.append('<li><a href="pubs.html">Publications</a></li>');
+    $navlist.append('<!--li><a href="sketchpad/index.html">Sketchpad</a></li-->');
+    $navlist.append('<li><a class="showInMobile" href="about.html">About</a></li>');
   }
 }
 
@@ -45,9 +51,10 @@ function detailUrl(title) {
 
 function createDetail(projects) {
 
+  selectNavigation();
+
   // create long text
-  var html = "";
-  var nav = "";
+  var html = "", nav = "";
   for (var i = 0; i < projects.length; i++) {
 
     html += "<div class='projectlong' id='" + detailUrl(projects[i].shorttitle) + "'><div class='grid'>";
@@ -55,8 +62,8 @@ function createDetail(projects) {
     html += "<div class='content clearfix'><div class='col-8-12 mobile-col-1-1 gap'>";
     html += "<h5>" + projects[i].longtitle + "</h5>";
     html += "<p class='longdesc'>" + projects[i].longdesc + "</p>";
-    
-    //QUOTE
+
+    // QUOTES
     if (projects[i].quotes) {
        html += "<div class='quotes'>";
       for (var j = 0; j < projects[i].quotes.length; j++) {
@@ -66,7 +73,8 @@ function createDetail(projects) {
       }
       html += "</div>";
     }
-    //EXHIBITION RECORD
+
+    // EXHIBITIONS
     if (projects[i].exhibitions) {
 
       html += "<ul class='exhibitions'>EXHIBITIONS";
@@ -80,30 +88,34 @@ function createDetail(projects) {
       html += "</ul>";
     }
 
-    //LINKS
+    // LINKS
     if (projects[i].links) {
 
       html += "<ul class='links'>LINKS";
       for (var j = 0; j < projects[i].links.length; j++) {
-        //<li><a href="">XXX</a></li>
-        html += "<li><a target='_blank' href='" + projects[i].links[j].target + "'>" + projects[i].links[j].name + "</a></li>";
+        html += "<li><a target='_blank' href='" + projects[i].links[j].target +
+          "'>" + projects[i].links[j].name + "</a></li>";
       }
       html += "</ul>";
     }
 
     html += "</div>";
     html += "<div class='col-1-3 mobile-col-1-1'><img src=" + projects[i].image + "></div></div></div>";
-   
-
     html += "<div class='bottomNav'>";
-    if (i != 0)
-        html += "<p><span>previous</span><a href='#" + detailUrl(projects[i - 1].shorttitle) + "'>" + projects[i - 1].longtitle + "</a></p>";
+
+    if (i != 0) {
+        html += "<p><span>previous</span><a href='#" + detailUrl(projects[i - 1].shorttitle) + "'>" +
+          projects[i - 1].longtitle + "</a></p>";
+    }
 
     if (i != projects.length - 1) {
-        html += "<p class='nextPage'><span>next</span><a href='#" + detailUrl(projects[i + 1].shorttitle) + "'>" + projects[i + 1].longtitle + "</a></p>";
+        html += "<p class='nextPage'><span>next</span><a href='#" +
+          detailUrl(projects[i + 1].shorttitle) + "'>" + projects[i + 1].longtitle + "</a></p>";
     }
+
     html += "</div></div>";
   }
+
   $('#detail').append(html);
 
   displayCurrent();
@@ -128,7 +140,7 @@ function displayCurrent() {
   var URL = $(location).attr('href');
   var id = URL.split("#")[1];
   var selector = '.projectlong#' + id;
-  console.log('displayCurrent:'+id,selector);
+  //console.log('displayCurrent:'+id,selector);
   $(".projectlong").css("display", "none");
   $(selector).css("display", "block");
 }
