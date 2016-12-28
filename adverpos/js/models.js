82b680b3f9be55806ba2a9176141cd760229e9a9
cqx931 <cqx931@gmail.com>
2016-12-28 16:16:11 +0800

    optimize adverpos images and implement zooming

--- adverpos/js/models.js
+++ adverpos/js/models.js
@@ -22,29 +22,39 @@ $(document).ready(function() {
       $('#nicole .items').css("color", "#80807F");
       $('video').hide();
       $('.intro').css("background-color", "rgba(0,0,0,0)");
+      $('img.zoom').css("visibility","hidden");
     })
     $('#detail').click(function() {
       $('#imageDisplay').removeClass().addClass(current + "Detail");
       $('video').hide();
       $('.items').css("color", "#EEE");
       $('.intro').css("background-color", "rgba(0,0,0,.5)");
+      $('img.zoom').css("visibility","hidden");
     })
     $('#adCollection').click(function() {
       $('#imageDisplay').removeClass().addClass(current + "AdCollection");
       $('video').hide();
       $('.items').css("color", "#EEE");
       $('.intro').css("background-color", "rgba(16,60,83,.8)");
+      $('img.zoom').css("visibility","hidden");
     })
     $('#animation').click(function() {
       $('#imageDisplay').removeClass();
        $('video').show();
       $('.items').css("color", "#EEE");
       $('.intro').css("background-color", "rgba(0,0,0,0)");
+      $('img.zoom').css("visibility","hidden");
     })
 
     $('#imageDisplay').click(function() {
-      $('img.zoom').width(window.innerWidth);
-      $('img.zoom').height(window.innerHeight);
+       $('img.zoom').css("visibility","visible");
+      
+       if ($('body').height() > 630) {
+           $('img.zoom').width($('body').width());
+           $('img.zoom').height($('body').height());
+       }
+       else $('img.zoom').height(630);
+    
     })
 
 
