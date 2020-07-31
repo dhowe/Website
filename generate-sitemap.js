adbbfb9fbeea68b410080b77e77acdeebf2392d2
dhowe <dhowe@cims.nyu.edu>
2020-07-31 16:16:14 +0800

    fix issues with sitemap-gen

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,17 +1,19 @@
-fs = require('fs')
-const { readdirSync } = require('fs')
+const fs = require('fs')
+const https = require('https');
+const projects = require('./projects.json');
 
 const WebsitePath = "https://rednoise.org/daniel/";
+const EventsUrl = "https://rednoise.org/wpr/wp-json/wp/v2/posts?categories=2&per_page=100";
 const TimeStamp = getTimeStamp();
 
 // Tools
 const getDirectories = source =>
-  readdirSync(source, { withFileTypes: true })
+  fs.readdirSync(source, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name)
 
 const getAll = source =>
-  readdirSync(source, { withFileTypes: true })
+  fs.readdirSync(source, { withFileTypes: true })
     .map(dirent => dirent.name)
 
 function detailUrl(title) {
@@ -25,11 +27,11 @@ function getTimeStamp() {
 
 function generateDetails() {
   const P = "0.70";
-  const prjs = require('./projects.json')
+  const prjs = projects;
   let result = ""
   for (let i = 0; i < prjs.length; i++) {
     // add images
-    result += "<url><loc>" + WebsitePath +  detailUrl(prjs[i].shorttitle) + "</loc><lastmod>" + TimeStamp + "</lastmod><priority>" + P + "</priority>\n";
+    result += "<url><loc>" + WebsitePath + detailUrl(prjs[i].shorttitle) + "</loc><lastmod>" + TimeStamp + "</lastmod><priority>" + P + "</priority>\n";
     result += !prjs[i].images ? "" : "  <image:image><image:loc>" + WebsitePath + prjs[i].images[0].src + "</image:loc></image:image>\n";
     result += "</url>\n";
   }
@@ -41,7 +43,7 @@ function generateProjectPages() {
   let result = ""
   const allPages = getDirectories("./pages");
   for (let i = 0; i < allPages.length; i++) {
-    result += "<url><loc>"+ WebsitePath + "pages/" + allPages[i] + "</loc><priority>0.80</priority></url>\n";
+    result += "<url><loc>" + WebsitePath + "pages/" + allPages[i] + "</loc><priority>0.80</priority></url>\n";
   }
   return result;
 }
@@ -49,49 +51,61 @@ function generateProjectPages() {
 function generateRoot() {
   const P = "0.80";
   // init with home page
-  let result = "<url><loc>" + WebsitePath + "</loc><lastmod>" + TimeStamp + "</lastmod><priority>1.00</priority></url>\n";
+  let result = "<url><loc>" + WebsitePath + "</loc><lastmod>"
+    + TimeStamp + "</lastmod><priority>1.00</priority></url>\n";
   const all = getAll("./");
   for (let i = 0; i < all.length; i++) {
     if (all[i] != "index.html" && all[i].indexOf(".html") > 0) {
-      result += "<url><loc>"+ WebsitePath + all[i] + "</loc><lastmod>" + TimeStamp + "</lastmod><priority>" + P + "</priority></url>\n";
+      result += "<url><loc>" + WebsitePath + all[i] + "</loc><lastmod>"
+        + TimeStamp + "</lastmod><priority>" + P + "</priority></url>\n";
     }
-}
-  return result;
-}
-
-async function generateWpr() {
-  const P = "0.51";
-  const EVENTS = "https://rednoise.org/wpr/wp-json/wp/v2/posts?categories=2&per_page=100";
-
-  async function getResult() {
-    const request = require("request-promise");
-    let result = "";
-    await request({url: EVENTS, json: true}, function(err, res, json) {
-        if (err) {
-          throw err;
-        }
-        json.forEach((post, i) => {
-          result += "<url><loc>"+ WebsitePath + post.link + "</loc><lastmod>" + TimeStamp + "</lastmod><priority>" + P + "</priority></url>\n";
-        });
-      });
-    return result;
   }
-
-  const result = await getResult();
   return result;
 }
 
-async function generateNewSiteMap() {
+async function doRequest(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (resp) => {
+      let data = '';
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+      resp.on('end', () => {
+        resolve(data);
+      });
+    }).on("error", (error) => {
+      reject(error);
+    });
+  });
+}
+
+async function generateSiteMap() {
   let newSiteMap = '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">';
 
   newSiteMap += generateRoot();
   newSiteMap += generateDetails();
-  newSiteMap += await generateWpr();
+  newSiteMap += await generateWpr()
   newSiteMap += "</urlset>";
 
-  fs.writeFile("sitemap.xml", newSiteMap, function(){
-    console.log("sitemap.xml has been updated!")
+  fs.writeFile("sitemap.xml", newSiteMap, function () {
+    console.log("sitemap.xml updated")
   });
 }
 
-generateNewSiteMap();
+async function generateWpr() {
+  let p = "0.51", result = '';
+  const raw = await doRequest(EventsUrl);
+  const json = JSON.parse(raw);
+  json.forEach((post) => {
+    result += "<url><loc>" + post.link + "</loc><lastmod>"
+      + TimeStamp + "</lastmod><priority>" + p + "</priority></url>\n";
+  });
+  return result;
+}
+
+/* 
+(async function() {
+  let json = 
+  console.log(json, 'done');
+})() */
+generateSiteMap();
